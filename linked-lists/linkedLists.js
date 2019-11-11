dccc9e105f287a405d7725969b868268f1b76ef8
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-10 18:52:48 -0800

    append/prepend

---
+++ linked-lists/linkedLists.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value, prev, next) {
+    this.value = value;
+    this.prev = prev || null;
+    this.next = next || null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = this.tail = null;
+  }
+
+  append(value) {
+    if(!this.tail) {
+      let oldTail = this.tail;
+      this.tail = new Node(value);
+      oldTail.next = this.tail;
+      this.tail.prev = oldTail;
+    }
+  }
+  
+  prepend(value) {
+    if(!this.head) {
+      this.head = this.tail = new Node(value);
+    } else {
+      let oldHead = this.head;
+      this.head = new Node(value);
+      oldHead.prev = this.head;
+      this.head.next = oldHead;
+    }
+  }
+}
+
+module.exports = { Node, LinkedList };
