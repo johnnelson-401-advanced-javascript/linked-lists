41f4691a8e8a12f75b147befadcec463e85bec33
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-10 19:18:34 -0800

    Prepends

--- __tests__/linked-list.test.js
+++ __tests__/linked-list.test.js
@@ -1,14 +1,23 @@
 const { LinkedList } = require('../linked-lists/linkedLists');
 
 describe('LinkedList methods', () => {
-  it('Appends, adds to the Head', () => {
+  it('Appends, adds to the tail', () => {
     const linkedList = new LinkedList;
     linkedList.append(1);
     linkedList.append(2);
     linkedList.append(3);
     console.log(linkedList.head);
 
-    expect(linkedList.head.value).toEqual(1);
+    expect(linkedList.tail.value).toEqual(3);
+  });
+
+  it('Prepends, adds to Head', () => {
+    const list = new LinkedList;
+    list.prepend(3);
+    list.prepend(2);
+    list.prepend(1);
+    expect(list.head.value).toEqual(1);
+
   });
 
 
