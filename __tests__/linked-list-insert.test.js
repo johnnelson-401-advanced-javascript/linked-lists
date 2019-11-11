73f782c69a2a624a5aabb76adc65d085531bb613
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-10 20:35:03 -0800

    throws errors when not a valid value

--- __tests__/linked-list-insert.test.js
+++ __tests__/linked-list-insert.test.js
@@ -9,7 +9,15 @@ describe('insert tests', () => {
     list.append('x');
     list.insertBefore(3, 5);
     expect(list.head.next.value).toBe(5);
-    
+  });
+
+  it('insertsBefore, throws an error if Value is not in the list', () => {
+    const list = new LinkedList;
+    list.append(1);
+    list.append(3);
+    list.append(2);
+    list.append('x');
+    expect(list.insertBefore(4, 5)).toBe(console.error('Value does not Exist'));
   });
 
   it('insertAfter, adds newVal to next of Value', () => {
@@ -19,10 +27,17 @@ describe('insert tests', () => {
     list.append(2);
     list.append('x');
     list.insertAfter(3, 5);
-    console.log(list);
     expect(list.head.next.next.value).toBe(5);
-    
-
   });
+
+  it('insertsAfter, throws an error if Value is not in the list', () => {
+    const list = new LinkedList;
+    list.append(1);
+    list.append(3);
+    list.append(2);
+    list.append('x');
+    expect(list.insertAfter(4, 5)).toBe(console.error('Value does not Exist'));
+  });
+
   
 });
--- linked-lists/linkedLists.js
+++ linked-lists/linkedLists.js
@@ -21,7 +21,7 @@ class LinkedList {
       this.tail.prev = oldTail;
     }
   }
-  
+
   prepend(value) {
     if(!this.head) {
       this.head = this.tail = new Node(value);
@@ -55,23 +55,30 @@ class LinkedList {
   }
 
   insertBefore(value, newVal) {
+    if(this.includes(value) === false){
+      return console.error('Value does not Exist');
+    }
     let current = this.head;
     let previous;
     while(current.value !== value) {
       previous = current;
       current = current.next;
     }
-    const newNode = new Node (newVal, current);
+    const newNode = new Node(newVal, current);
     previous.next = newNode;
   }
   insertAfter(value, newVal) {
+    if(this.includes(value) === false){
+      return console.error('Value does not Exist');
+    }
     let current = this.head;
     while(current.value !== value) {
       current = current.next;
     }
     let next = current.next;
-    const newNode = new Node (newVal, next);
+    const newNode = new Node(newVal, next);
     current.next = newNode;
+    
   }
 
 
