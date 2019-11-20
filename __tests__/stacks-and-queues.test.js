265cbb86e7b26f280100e93d276074a70df9b09a
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-19 22:19:39 -0800

    Stack methods tested, with error reporting

--- __tests__/stacks-and-queues.test.js
+++ __tests__/stacks-and-queues.test.js
@@ -1,13 +1,35 @@
 const { Stack } = require('../stack-and-queue/stacks-and-queues');
 
-describe('Stack class tests', () => {
+const stack = new Stack;
+stack.push('1');
+stack.push('2');
+stack.push('3');
 
+
+describe('Stack class tests', () => {
+  
   it('adds elements to a stack in the order they are placed', () => {
-    const stack = new Stack;
-    stack.push('1');
-    stack.push('2');
-    stack.push('3');
+   
     expect(stack).toEqual({ size: 3, storage: { 1: '1', 2: '2', 3: '3' } });
+  });
+  it('Peeks at the last element in the stack.', () => {
+    let peekReturn = stack.peek();
+    expect(peekReturn).toEqual('3');
+  });
+  it('Pops the last element off the stack and returns it.', () => {
+    let popReturn = stack.pop();
+    expect(popReturn).toEqual('3');
+    expect(stack).toEqual({ size: 2, storage: { 1: '1', 2: '2' } });
+  });
+  it('Checks the isEmpty method', () => {
+    expect(stack.isEmpty()).toBe(false);
+    stack.pop();
+    stack.pop();
+    expect(stack.isEmpty()).toBe(true);
+    function err() {
+      stack.pop();
+    }
+    expect(err).toThrowError();
   });
   
 });
--- stack-and-queue/stacks-and-queues.js
+++ stack-and-queue/stacks-and-queues.js
@@ -10,14 +10,25 @@ class Stack {
   }
 
   pop() {
-    let removed = this.storage[this.size];
-    delete this.storage[this.size];
-    this.size--;
-    return removed;
+    if(this.isEmpty() === false) {
+      let removed = this.storage[this.size];
+      delete this.storage[this.size];
+      this.size--;
+      return removed;
+    }
+    else throw console.error('Stack is Empty');
   }
 
   peek() {
     return this.storage[this.size];
+  }
+
+  isEmpty() {
+    if(this.size === 0) {
+      return true;
+    } else if(this.size > 0) {
+      return false;
+    } else throw console.error('Input is not a valid Stack');
   }
 
 
@@ -25,4 +36,4 @@ class Stack {
 
 module.exports = {
   Stack
-}
+};
